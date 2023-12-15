56d97eabd59da0bc558d24b33047d4ea115a34d6
cuongbrave <nguyenmanhcuongk10hv@gmail.com>
2023-12-15 16:11:09 +0700

    first commit

---
+++ lesson1.js
@@ -0,0 +1,12 @@
+// console.log('kteam');
+// console.log(1234);
+// console.log(2+4);
+{
+    let t = 'name';
+    
+}
+//console.log(t);
+let a, b, c;
+[a, b, c] = [1, 2, 3];
+console.log(a, b, c);
+console.log(typeof(a));
